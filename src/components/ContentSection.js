08281a2d5cd17f9ded33cfed9dec2aa47b19bc56
Sajan Acharya <sajanacharya20@gmail.com>
2022-11-04 14:20:59 -0400

    checking at 2.20 PM

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -9,7 +9,7 @@ function ContentSection() {
   return (
     <div className='content-section-container'>
         <video src= {homevideo} autoPlay loop muted/>
-        <h1>Welcome Caregiver "checking now again" </h1>
+        <h1>Welcome Caregiver "checking at 2.20 PM" </h1>
         <p>Please Login</p>
         <div className='content-section-btns'>
             <Button className='sign-up' buttonStyle='btn--outline' buttonSize='btn--large'> <Link to="/login" className='nav-links-alt'>
